626eb5d3274f00cde2f0ec2411ce974d6ceaf26c
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2021-10-02 20:07:07 +0200

    Add fancy overtime fixing

--- Unity Project/LD49_JAMGAME/Assets/Scripts/Gameplay/Bugs/TwoSeperateObjectsBug/TwoSeperateObjectsBugIncorrect.cs
+++ Unity Project/LD49_JAMGAME/Assets/Scripts/Gameplay/Bugs/TwoSeperateObjectsBug/TwoSeperateObjectsBugIncorrect.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class TwoSeperateObjectsBugIncorrect : MonoBehaviour
 {
     public TwoSeperateObjectsBug parent;
+    public TwoSeperateObjectsBugCorrect CorrectObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +14,42 @@ public class TwoSeperateObjectsBugIncorrect : MonoBehaviour
         {
             parent = GetComponentInParent<TwoSeperateObjectsBug>();
         }
+        CorrectObject = parent.CorrectObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void StartFixing()
+    {
+        StartCoroutine(MoveOverSeconds(parent.CorrectObject, parent.MaxFixTime));
+    }
+
+    public IEnumerator MoveOverSeconds(TwoSeperateObjectsBugCorrect destinationObject, float seconds)
+    {
+        float elapsedTime = 0;
+
+        Vector3 startPosition = gameObject.transform.position;
+        Vector3 endPosition = destinationObject.transform.position;
+
+        Vector3 startScale = gameObject.transform.localScale;
+        Vector3 endScale = destinationObject.transform.localScale;
+
+        Quaternion startRotation = gameObject.transform.rotation;
+        Quaternion endRotation = destinationObject.transform.rotation;
+
+        while (elapsedTime < seconds)
+        {
+            gameObject.transform.position = Vector3.Lerp(startPosition, endPosition, (elapsedTime / seconds));
+            gameObject.transform.localScale = Vector3.Lerp(startScale, endScale, (elapsedTime / seconds));
+            gameObject.transform.rotation = Quaternion.Lerp(startRotation, endRotation, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        gameObject.transform.position = endPosition;
+        gameObject.transform.localScale = endScale;
+        gameObject.transform.rotation = endRotation;
     }
 }
