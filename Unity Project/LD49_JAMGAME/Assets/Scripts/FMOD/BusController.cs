86b09b2289d1d0ae0071dac2a715c39ba103a4a5
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-10-03 12:35:56 +0200

    volume slider should stay at the correct value now

--- Unity Project/LD49_JAMGAME/Assets/Scripts/FMOD/BusController.cs
+++ Unity Project/LD49_JAMGAME/Assets/Scripts/FMOD/BusController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using FMOD.Studio;
 using FMODUnity;
+using UnityEngine.UI;
 
 public class BusController : MonoBehaviour
 {
 
     Bus Bus;
     public string BusPath;
+    private float BusVolume;
+    private float FinalBusVolume;
+    private Slider Slider;
 
     // Start is called before the first frame update
     void Start()
     {
         Bus = RuntimeManager.GetBus("bus:/" + BusPath);
+        Bus.getVolume(out BusVolume, out FinalBusVolume);
+
+        Slider = GetComponent<Slider>();
+        Slider.value = BusVolume;
     }
 
     public void VolumeLevel (float SliderValue)
