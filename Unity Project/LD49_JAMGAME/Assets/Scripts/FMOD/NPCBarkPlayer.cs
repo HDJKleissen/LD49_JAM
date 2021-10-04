ba696ae55a1209e0a2ecfff8dbcfd55c95ea68e5
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-10-04 14:59:49 +0200

    tied animation to talking BUT it''s hardcoded and dumb

--- Unity Project/LD49_JAMGAME/Assets/Scripts/FMOD/NPCBarkPlayer.cs
+++ Unity Project/LD49_JAMGAME/Assets/Scripts/FMOD/NPCBarkPlayer.cs
@@ -7,25 +7,57 @@ public class NPCBarkPlayer : MonoBehaviour
     [SerializeField] float ShortestTimeBetweenBarks = 15f;
     [SerializeField] float LongestTimeBetweenBarks = 25f;
     public bool isBug;
+    private NPC NPC;
+    private FMOD.Studio.EventInstance VOLine;
+    private FMOD.Studio.EventInstance BuggedVOLine;
+    private FMOD.Studio.PLAYBACK_STATE VOLinePs;
+    private FMOD.Studio.PLAYBACK_STATE BuggedVOLinePs;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        VOLine = FMODUnity.RuntimeManager.CreateInstance("event:/NPC_Bark");
+        BuggedVOLine = FMODUnity.RuntimeManager.CreateInstance("event:/NPC_Bark_Bug");
+        NPC = GetComponent<NPC>();
         Invoke("PlayBark", Random.Range(ShortestTimeBetweenBarks, LongestTimeBetweenBarks));
     }
 
 
-    void PlayBark()
+    private void Update()
     {
-        if (isBug)
+        VOLine.getPlaybackState(out VOLinePs);
+        BuggedVOLine.getPlaybackState(out BuggedVOLinePs);
+        if(VOLinePs == FMOD.Studio.PLAYBACK_STATE.PLAYING || BuggedVOLinePs == FMOD.Studio.PLAYBACK_STATE.PLAYING )
         {
-            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NPC_Bark_Bug", gameObject);
+            NPC.CurrentState = NPCState.Talking;
         }
         else
         {
-            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/NPC_Bark", gameObject);
+            NPC.CurrentState = NPCState.Idle;
+        }
+    }
+
+    void PlayBark()
+    {
+        
+        if (isBug)
+        {
+            BuggedVOLine.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+            BuggedVOLine.start();
+        }
+        else
+        {
+            VOLine.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform));
+            VOLine.start();
         }
 
         Invoke("PlayBark", Random.Range(ShortestTimeBetweenBarks, LongestTimeBetweenBarks));
+    }
+
+    private void OnDestroy()
+    {
+        VOLine.release();
+        BuggedVOLine.release();
     }
 }
